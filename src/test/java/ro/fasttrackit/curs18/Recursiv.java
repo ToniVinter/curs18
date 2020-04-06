acdbc55e87a23eec9e7c4dcba16e7eca3719ef4f
Vinter Antonio <tonyivinter@gmail.com>
2020-04-06 18:46:11 +0300

    ifNullThenException

---
+++ src/test/java/ro/fasttrackit/curs18/Recursiv.java
@@ -0,0 +1,19 @@
+package ro.fasttrackit.curs18;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Recursiv {
+    Recursive recursiv;
+    @BeforeEach
+    void setup(){
+        recursiv = new Recursive();
+    }
+    @Test
+    @DisplayName("WHEN null is received THEN throw exception")
+    void ifNullThenException(){
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> recursiv.length(null));
+    }
+}
